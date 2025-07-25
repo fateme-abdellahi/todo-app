944c482fc791f185903ebc0c4b816ad449ad8596
Fateme Abdellahi <fateme.abdellahii@gmail.com>
2025-07-25 13:40:56 +0330

    permission class added

--- todo/todo_app/api/views.py
+++ todo/todo_app/api/views.py
@@ -1,9 +1,10 @@
-from rest_framework import generics
+from rest_framework import generics,permissions
 from . import serializers
 from .. import models
 
 
 class ListToDOItemView(generics.ListAPIView):
+    permission_classes=[permissions.IsAuthenticated]
     serializer_class=serializers.ItemSerializer
     
     def get_queryset(self):
