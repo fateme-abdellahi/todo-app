387373cfcbcc0ea17e2e5b18b009ca4b763a910a
Fateme Abdellahi <89656427+fateme-abdellahi@users.noreply.github.com>
2025-10-17 12:25:33 +0330

    Delete todo directory

--- todo/authenticate/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from rest_framework.authtoken.views import obtain_auth_token
-
-urlpatterns = [
-    path('register/', views.RegisterView.as_view(), name='register'),
-    path('login/', obtain_auth_token, name='login'),
-    path('logout/', views.LogoutView.as_view(), name='logout'),
-]
--- todo/todo_app/api/paginations.py
+++
@@ -1,4 +0,0 @@
-from rest_framework.pagination import PageNumberPagination
-
-class ToDOListPagination(PageNumberPagination):
-    page_size_query_param='limit'
